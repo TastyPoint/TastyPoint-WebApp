5b7ed0d9b0ff1b8799146193d278d36b507e72d5
FaridIzquierdo <u202015215@upc.edu.pe>
2023-09-29 01:36:53 -0500

    Add Register section, also router module was also added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -9,18 +11,21 @@ import { MaterialModule } from 'shared/material.module';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarLoginComponent } from './components/navbar-login/navbar-login.component';
 import { FooterLoginComponent } from './components/footer-login/footer-login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NavbarLoginComponent,
-    FooterLoginComponent
+    FooterLoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
