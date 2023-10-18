52aa83246ad9cdcafc57cc56b6887cb97c192bde
FaridIzquierdo <u202015215@upc.edu.pe>
2023-10-18 15:27:19 -0500

    Add Section update-inventory and update routing module

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,12 +6,16 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { canActivate, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+import { RegisterInventoryComponent } from './components/register-inventory/register-inventory.component';
+import { UpdateInventoryComponent } from './components/update-inventory/update-inventory.component';
 
 const routes: Routes = [
-    {path: '', pathMatch: 'full', redirectTo: 'login'},
-    {path: 'home', component: HomeComponent, ...canActivate(()=> redirectUnauthorizedTo(['/login'])) },
+    {path: '', pathMatch: 'full', redirectTo: 'login'}, 
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'home', component: HomeComponent, ...canActivate(()=> redirectUnauthorizedTo(['/login'])) },
+    {path: 'register-inventory', component: RegisterInventoryComponent, ...canActivate(()=> redirectUnauthorizedTo(['/login'])) },
+    {path: 'update-inventory', component: UpdateInventoryComponent, ...canActivate(()=> redirectUnauthorizedTo(['/login'])) }
 ];
 
 @NgModule({
