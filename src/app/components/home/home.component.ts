82545224eb85ae7db3fbb80c35f03fded4134c48
MechaXAI <adrianxalzamora@gmail.com>
2023-11-04 11:47:00 -0500

    feat: updated ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -41,11 +41,27 @@ export class HomeComponent {
     avg_hum: 0
   };
 
-  messages = {
-    danger: 'The warehouse is temperature is above the limit',
-    warning: 'The warehouse is temperature is close to the limit',
-    success: 'The warehouse is temperature is below the limit'
+  ms = {
+    danger: 'The warehouse temperature is above the acceptable limit to store food',
+    warning: 'The warehouse temperature is close to the limit to store food',
+    success: 'The warehouse temperature is below the limit to store food'
   };
+
+  msHm = {
+    danger: 'The warehouse humidity is above the acceptable limit to store food',
+    warning: 'The warehouse humidity is close to the acceptable limit store food',
+    success: 'The warehouse humidity is below the acceptable limit to store food'
+  };
+
+  tempIcons = {
+    danger: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSnQ11PBlabnP8fLSpllixv-eYZPKjbfwmsJLaZDQeY6MTalRO',
+    warning: 'https://www.clipartmax.com/png/middle/226-2260634_temperature-png-room-temperature-icon-png.png',
+    success: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEriCj_jk9rVosAQGdT4rHXpyMPW17kKrx2w&usqp=CAU'
+  }
+  
+  tempIcon = this.tempIcons.success;
+  tempMs = "";
+  humMs = "";
 
   private database: Database = inject(Database);
 
@@ -63,11 +79,16 @@ export class HomeComponent {
       if (snapshot.exists()) {
         this.warehouse.temperature = readings[Object.keys(readings)[0]].temperature;
         this.warehouse.humidity = Math.floor(readings[Object.keys(readings)[0]].humidity);
+        this.tempMs = this.getTempMessage(this.warehouse.temperature);
+        this.humMs = this.getHumMessage(this.warehouse.humidity);
+        this.tempIcon = this.getTempIcon(this.warehouse.temperature);
       } else {
         this.warehouse.temperature = 0;
         this.warehouse.humidity = 0;
       }
     });
+
+    
   }
 
   getMaxTemperature() {
@@ -107,7 +128,7 @@ export class HomeComponent {
 
     onValue(minReadingTemp, snapshot => {
       const readings = snapshot.val();
-     
+
       if (snapshot.exists()) {
         this.warehouse.min_temp = readings[Object.keys(readings)[0]].temperature;
       } else {
@@ -117,7 +138,7 @@ export class HomeComponent {
 
     onValue(minReadingHumd, snapshot => {
       const readings = snapshot.val();
-      
+
       if (snapshot.exists()) {
         this.warehouse.min_hum = readings[Object.keys(readings)[0]].humidity;
       } else {
@@ -147,9 +168,36 @@ export class HomeComponent {
     });
   }
 
+  getTempMessage(temp: number):string {
+    if (temp >= -10 && temp <= 10) {
+      return this.ms.success;
+    } else if (temp > 10 && temp <= 20) {
+      return this.ms.warning;
+    } else {
+      return this.ms.danger;
+    } 
+  }
+  getHumMessage(hum: number):string {
+    if (hum >= 50 && hum <= 55) {
+      return this.msHm.success;
+    } else if (hum > 55 && hum <= 60) {
+      return this.msHm.warning;
+    } else {
+      return this.msHm.danger;
+    } 
+  }
+  getTempIcon(temp: number): string {
+    if (temp >= -10 && temp <= 10) {
+      return this.tempIcons.success;
+    } else if (temp > 10 && temp <= 20) {
+      return this.tempIcons.warning;
+    } else {
+      return this.tempIcons.danger;
+    }
+  }
   ngOnInit(): void {
     this.data.uid = this.userService.getUserUid();
- 
+
     this.getData();
     this.getMaxTemperature();
     this.getMinTempAndHum();
